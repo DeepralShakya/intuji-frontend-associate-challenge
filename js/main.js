70d6495f5e3168f9d951e8d499455c066aac1960
DeepralShakya <shakyadeepral44@gmail.com>
2025-03-29 00:24:10 +0545

    Header Added

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 async function loadContent() {
-    document.getElementById("sidebar").innerHTML = await fetch('../sidebar.html').then(res => res.text());
+    try {
+        document.getElementById("sidebar").innerHTML = await fetch('../sidebar.html').then(res => res.text());
+
+        document.getElementById("main-content").innerHTML = await fetch('../main-content.html').then(res => res.text());
+
+        document.getElementById("header").innerHTML = await fetch('../header.html').then(res => res.text());
+    } catch (error) {
+        console.error('Error loading content:', error);
+    }
 }
 
 loadContent();
