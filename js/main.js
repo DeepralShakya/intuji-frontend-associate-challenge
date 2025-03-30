6d70d98a98ec2d57563ef75324b40db8979254d1
DeepralShakya <shakyadeepral44@gmail.com>
2025-03-30 14:40:53 +0545

    Recent Transaction Added and some adjustments

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 async function loadContent() {
     try {
-        document.getElementById("sidebar").innerHTML = await fetch('../sidebar.html').then(res => res.text());
-        document.getElementById("main-content").innerHTML = await fetch('../main-content.html').then(res => res.text());
-        document.getElementById("header").innerHTML = await fetch('../header.html').then(res => res.text());
-        document.getElementById("overview").innerHTML = await fetch('../overview.html').then(res => res.text());
-        document.getElementById("saving-plan").innerHTML = await fetch('../saving-plan.html').then(res => res.text());
-        document.getElementById("analytics").innerHTML = await fetch('../analytics.html').then(res => res.text());
-        document.getElementById("recent-transaction.").innerHTML = await fetch('../recent-transaction..html').then(res => res.text());
+        document.getElementById("sidebar").innerHTML = await fetch('components/sidebar.html').then(res => res.text());
+        document.getElementById("main-content").innerHTML = await fetch('components/main-content.html').then(res => res.text());
+        document.getElementById("header").innerHTML = await fetch('components/header.html').then(res => res.text());
+        document.getElementById("overview").innerHTML = await fetch('components/overview.html').then(res => res.text());
+        document.getElementById("saving-plan").innerHTML = await fetch('components/saving-plan.html').then(res => res.text());
+        document.getElementById("analytics").innerHTML = await fetch('components/analytics.html').then(res => res.text());
+        document.getElementById("recent-transaction").innerHTML = await fetch('components/recent-transaction.html').then(res => res.text());
     } catch (error) {
         console.error('Error loading content:', error);
     }
